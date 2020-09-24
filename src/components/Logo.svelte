38530331d1d5551a98c5f6e3d35c10ba56b7208c
Naveen <catchspider2002@gmail.com>
2020-09-24 11:15:12 +0530

    Updated logo and favicon

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -8,23 +8,11 @@
 
 <a href="/">
   <!-- <img alt="Sapper" src='logo-192.png'> -->
-
-  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 64 64">
-    <g fill="none" stroke="#000" stroke-width="2" data-name="computer coding">
-      <path d="M35.251 25.85L28.75 50.167" stroke-linecap="round" />
-      <path
-        d="M40.201 29.511l6.52 6.507a2.807 2.807 0 01-.013 3.993l-6.507 6.494M23.799 46.505L17.279 40a2.807 2.807 0 01.013-3.993l6.507-6.495"
-        color="#000"
-        overflow="visible"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        paint-order="markers stroke fill" />
-      <path
-        d="M6.5 6.466h51c1.662 0 3 1.34 3 3.005v45.057a2.996 2.996 0 01-3 3.006h-51c-1.662 0-3-1.34-3-3.006V9.471a2.996 2.996 0 013-3.005zm-3 11.979h57"
-        color="#000"
-        overflow="visible"
-        stroke-linejoin="round"
-        paint-order="markers stroke fill" />
+  <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 0 64 64" width="32">
+    <g fill="none" stroke="#fff" stroke-width="6" data-name="computer coding">
+      <path d="M35.018 27.5L28.982 50" stroke-linecap="round"/>
+      <path d="M44.016 30.888l6.051 6.02a2.592 2.592 0 01-.012 3.695l-6.039 6.009M19.984 46.612l-6.051-6.02a2.592 2.592 0 01.012-3.695l6.039-6.009" color="#000" overflow="visible" stroke-linecap="round" stroke-linejoin="round" paint-order="markers stroke fill"/>
+      <path d="M7.395 4.5h49.21c1.604 0 2.895 1.444 2.895 3.237v48.526c0 1.793-1.291 3.237-2.895 3.237H7.395C5.79 59.5 4.5 58.056 4.5 56.263V7.737C4.5 5.944 5.791 4.5 7.395 4.5zM4.5 18h55" color="#000" overflow="visible" stroke-linejoin="round" paint-order="markers stroke fill"/>
     </g>
   </svg>
 </a>
