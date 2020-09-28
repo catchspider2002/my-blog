1430c627c157d18cd4c0a1e8cc4aa2b4dfdcf59a
Naveen <catchspider2002@gmail.com>
2020-09-29 02:46:39 +0530

    Updated colors

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -12,18 +12,16 @@
 <a href="/">
   <!-- <img alt="Sapper" src='logo-192.png'> -->
   <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 0 64 64" width="32">
-    <g fill="none" stroke="#fff" stroke-width="6" data-name="computer coding">
+    <g fill="none" stroke="#000" stroke-width="6" data-name="computer coding">
       <path d="M35.018 27.5L28.982 50" stroke-linecap="round" />
       <path
         d="M44.016 30.888l6.051 6.02a2.592 2.592 0 01-.012 3.695l-6.039 6.009M19.984 46.612l-6.051-6.02a2.592 2.592 0 01.012-3.695l6.039-6.009"
-        color="#000"
         overflow="visible"
         stroke-linecap="round"
         stroke-linejoin="round"
         paint-order="markers stroke fill" />
       <path
         d="M7.395 4.5h49.21c1.604 0 2.895 1.444 2.895 3.237v48.526c0 1.793-1.291 3.237-2.895 3.237H7.395C5.79 59.5 4.5 58.056 4.5 56.263V7.737C4.5 5.944 5.791 4.5 7.395 4.5zM4.5 18h55"
-        color="#000"
         overflow="visible"
         stroke-linejoin="round"
         paint-order="markers stroke fill" />
